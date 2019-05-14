f5ce1b57ae44cba400fc04853eaefd88458b0ef6
Guilherme Silva <32681069+gcsilva11@users.noreply.github.com>
2019-05-14 22:26:57 +0100

    THATS ALOTTA ITEMS
    It's gonna be a long night

--- codenameGOD/main/game/item/items.lua
+++ codenameGOD/main/game/item/items.lua
@@ -15,7 +15,7 @@ return {
 	coins = {
 		name = "coins",
 		text = "Coins!",
-		position = {x=610, y=214},
+		position = {x=380, y=214},
 		image = "items2",
 		scenario = "room",
 		status = "hide",
@@ -25,7 +25,7 @@ return {
 	coins_stub1 = {
 		name = "coins_s1",
 		text = "stub text!",
-		position = {x=670, y=214},
+		position = {x=410, y=214},
 		image = "items2",
 		scenario = "room",
 		status = "hide",
@@ -35,7 +35,77 @@ return {
 	coins_stub2 = {
 		name = "coins_s2",
 		text = "stub text 2!",
-		position = {x=520, y=214},
+		position = {x=440, y=214},
+		image = "items2",
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "coins"
+	},
+	coins_stub3 = {
+		name = "coins_s3",
+		text = "stub text 2!",
+		position = {x=470, y=214},
+		image = "items2",
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "coins"
+	},
+	coins_stub4 = {
+		name = "coins_s4",
+		text = "stub text 2!",
+		position = {x=500, y=214},
+		image = "items2",
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "coins"
+	},
+	coins_stub5 = {
+		name = "coins_s5",
+		text = "stub text 2!",
+		position = {x=530, y=214},
+		image = "items2",
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "coins"
+	},
+	coins_stub6 = {
+		name = "coins_s6",
+		text = "stub text 2!",
+		position = {x=560, y=214},
+		image = "items2",
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "coins"
+	},
+	coins_stub7 = {
+		name = "coins_s7",
+		text = "stub text 2!",
+		position = {x=590, y=214},
+		image = "items2",
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "coins"
+	},
+	coins_stub8 = {
+		name = "coins_s8",
+		text = "stub text 2!",
+		position = {x=620, y=214},
+		image = "items2",
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "coins"
+	},
+	coins_stub9 = {
+		name = "coins_s9",
+		text = "stub text 2!",
+		position = {x=650, y=214},
 		image = "items2",
 		scenario = "room",
 		status = "hide",
@@ -43,7 +113,7 @@ return {
 		stub_for = "coins"
 	},
 	ticket = {
-		name = "",
+		name = "ticket",
 		text = "Ticket",
 		position = {x=262, y=375},
 		image = "items3",
@@ -51,6 +121,46 @@ return {
 		status = "hide",
 		found = 0,
 		stub_for = "self"
+	},
+	ticket_s = {
+		name = "ticket_s",
+		text = "Ticket",
+		position = {x=212, y=375},
+		image = "items3",
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "ticket"
+	},
+	ticket_s2 = {
+		name = "ticket_s",
+		text = "Ticket",
+		position = {x=162, y=375},
+		image = "items3",
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "ticket"
+	},
+	ticket_s3 = {
+		name = "ticket_s",
+		text = "Ticket",
+		position = {x=112, y=375},
+		image = "items3",
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "ticket"
+	},
+	ticket_s4 = {
+		name = "ticket_s",
+		text = "Ticket",
+		position = {x=72, y=375},
+		image = "items3",
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "ticket"
 	},
 	shoe1 = {
 		name = "",
