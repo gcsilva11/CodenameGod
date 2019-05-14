d7beba9f2688a14de7661e2fe4bdea8c868009a7
Solange Santos <solange-santos-851@hotmail.com>
2019-05-14 05:57:12 +0100

    Criação de items passou a ser gerida globalmente. Passou a ser usado um mapa de items para gerir os items. Quando se sai de um cenário para outro, os items mantêm o seu estado. Refactor.

---
+++ codenameGOD/main/game/item/items.lua
@@ -0,0 +1,32 @@
+return {
+	petrock = {
+		text = "Nearly forgot my pet rock!",
+		position = {x=984, y=224},
+		image = "items1",
+		scenario = "room"
+	},
+	coins = {
+		text = "Found coins!",
+		position = {x=650, y=214},
+		image = "items2",
+		scenario = "room"
+	},
+	ticket = {
+		text = "That's where it was!",
+		position = {x=262, y=375},
+		image = "items3",
+		scenario = "room"
+	},
+	shoe1 = {
+		text = "Ah there's one...",
+		position = {x=262, y=98},
+		image = "items4",
+		scenario = "room"
+	},
+	shoe2 = {
+		text = "Found a shoe!",
+		position = {x=814, y=159},
+		image = "items5",
+		scenario = "room"
+	}
+}
