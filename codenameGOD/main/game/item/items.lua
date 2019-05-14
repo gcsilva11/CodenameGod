07e27cd49d01ea0e6257ecf482033f5e36f30b7c
Guilherme Silva <32681069+gcsilva11@users.noreply.github.com>
2019-05-14 22:00:06 +0100

    wuup
    wuup

--- codenameGOD/main/game/item/items.lua
+++ codenameGOD/main/game/item/items.lua
@@ -1,32 +1,76 @@
 return {
+
+	-- ROOM ITEMS --
+	
 	petrock = {
-		text = "Nearly forgot my pet rock!",
+		name = "petrock",
+		text = "Pet rock!",
 		position = {x=984, y=224},
 		image = "items1",
-		scenario = "room"
+		scenario = "room",
+		status = "show",
+		found = 0,
+		stub_for = "self"
 	},
 	coins = {
-		text = "Found coins!",
-		position = {x=650, y=214},
+		name = "coins",
+		text = "Coins!",
+		position = {x=610, y=214},
 		image = "items2",
-		scenario = "room"
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "self"
+	},
+	coins_stub1 = {
+		name = "coins_s1",
+		text = "stub text!",
+		position = {x=670, y=214},
+		image = "items2",
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "coins"
+	},
+	coins_stub2 = {
+		name = "coins_s2",
+		text = "stub text 2!",
+		position = {x=520, y=214},
+		image = "items2",
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "coins"
 	},
 	ticket = {
-		text = "That's where it was!",
+		name = "",
+		text = "Ticket",
 		position = {x=262, y=375},
 		image = "items3",
-		scenario = "room"
+		scenario = "room",
+		status = "hide",
+		found = 0,
+		stub_for = "self"
 	},
 	shoe1 = {
-		text = "Ah there's one...",
+		name = "",
+		text = "Shoe",
 		position = {x=262, y=98},
 		image = "items4",
-		scenario = "room"
+		scenario = "room",
+		status = "show",
+		found = 0,
+		stub_for = "self"
 	},
 	shoe2 = {
-		text = "Found a shoe!",
+		name = "",
+		text = "Shoe",
 		position = {x=814, y=159},
 		image = "items5",
-		scenario = "room"
+		scenario = "room",
+		status = "show",
+		found = 0,
+		stub_for = "self"
 	}
+	-- END OF ROOM ITEMS
 }
