dc4f9b6c9958f72364e9ef53b3f2a88a99e5adb4
Solange Santos <solange-santos-851@hotmail.com>
2019-05-29 03:08:37 +0100

    Adicionado som de diálogos. Alguns bug fixes.

---
+++ codenameGOD/main/game/dialogue/dialogue.go
@@ -0,0 +1,270 @@
+components {
+  id: "dialogue"
+  component: "/main/game/dialogue/dialogue.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Bullies_1"
+  component: "/assets/sounds/dialogue/Bullies_1.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Bullies_2"
+  component: "/assets/sounds/dialogue/Bullies_2.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Bullies_3"
+  component: "/assets/sounds/dialogue/Bullies_3.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Bullies_4"
+  component: "/assets/sounds/dialogue/Bullies_4.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Bullies_5"
+  component: "/assets/sounds/dialogue/Bullies_5.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Randall_1"
+  component: "/assets/sounds/dialogue/Randall_1.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Randall_2"
+  component: "/assets/sounds/dialogue/Randall_2.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Randall_3"
+  component: "/assets/sounds/dialogue/Randall_3.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Randall_4"
+  component: "/assets/sounds/dialogue/Randall_4.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Randall_5"
+  component: "/assets/sounds/dialogue/Randall_5.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Randall_6"
+  component: "/assets/sounds/dialogue/Randall_6.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Randall_panic1"
+  component: "/assets/sounds/dialogue/Randall_panic1.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Randall_panic2"
+  component: "/assets/sounds/dialogue/Randall_panic2.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Randall_short1"
+  component: "/assets/sounds/dialogue/Randall_short1.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Randall_short2"
+  component: "/assets/sounds/dialogue/Randall_short2.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Randall_short3"
+  component: "/assets/sounds/dialogue/Randall_short3.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "Randall_short4"
+  component: "/assets/sounds/dialogue/Randall_short4.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
